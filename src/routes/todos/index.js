988b5d325f800a24287ac2705209d7fcbdbe6b11
techmocker <kevin.losem@docc.techstarter.de>
2024-02-21 10:53:23 +0100

    Update index.js

--- src/routes/todos/index.js
+++ src/routes/todos/index.js
@@ -22,12 +22,41 @@ TodosRouter.get("/all", (req, res) => {
   res.status(StatusCodes.OK).json(todos); // Annahme: todos ist dein Array mit Todos
 });
 
+
+TodosRouter.get("/byuserid", (req, res) => {
+  const userId = req.query.userId;
+  if (!userId) {
+    res.status(StatusCodes.BAD_REQUEST).send(ReasonPhrases.BAD_REQUEST);
+    return;
+  }
+  res.status(StatusCodes.OK).send("Get Todo by user id");
+});
+
 // PUT REQUESTS
 TodosRouter.put("/mark", (req, res) => {
   res.status(StatusCodes.OK).send("Todo als erledeigt markieren");
 });
 TodosRouter.put("/update", (req, res) => {
   res.status(StatusCodes.OK).send("Todo aktuallisieren");
+});
+
+TodosRouter.put("/mark", (req, res) => {
+  const todoId = req.body.todoId;
+  if (!todoId) {
+    res.status(StatusCodes.BAD_REQUEST).send(ReasonPhrases.BAD_REQUEST);
+    return;
+  }
+  res.status(StatusCodes.OK).send("Todo als erledigt markieren");
+});
+
+TodosRouter.put("/update", (req, res) => {
+  const todoId = req.body.todoId;
+  const updatedTodo = req.body.updatedTodo;
+  if (!todoId || !updatedTodo) {
+    res.status(StatusCodes.BAD_REQUEST).send(ReasonPhrases.BAD_REQUEST);
+    return;
+  }
+  res.status(StatusCodes.OK).send("Todo aktualisieren");
 });
 
 // POST REQUESTS
@@ -43,6 +72,15 @@ TodosRouter.post("/todo", (req, res) => {
 // DELETE REQUEST
 TodosRouter.delete("/delete", (req, res) => {
   res.status(StatusCodes.OK).send("DELTE Todo");
+});
+
+TodosRouter.delete("/delete", (req, res) => {
+  const todoId = req.query.todoId;
+  if (!todoId) {
+    res.status(StatusCodes.BAD_REQUEST).send(ReasonPhrases.BAD_REQUEST);
+    return;
+  }
+  res.status(StatusCodes.OK).send("DELETE Todo");
 });
 
 module.exports = { TodosRouter };
